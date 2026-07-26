8a2e5acc4002a2b8ddc2a0fdbf930c978dfa712f
Jon Thysell <thysell@gmail.com>
2026-07-26 14:32:38 -0700

    Fix typo with ʻai definition

--- src/HawDict/Input/PukuiElbertInputDict.cs
+++ src/HawDict/Input/PukuiElbertInputDict.cs
@@ -167,6 +167,7 @@ namespace HawDict
                 .Replace("<span lang=\"HAW\">&#699;Ana&#699;anapu'u ka uila", "<span lang=\"HAW\">&#699;Ana&#699;anapu&#699;u ka uila")
                 .Replace("<span lang=\"HAW\">Ka-p&#363;,lehu", "<span lang=\"HAW\">Ka-p&#363;.lehu")
                 .Replace("<span lang=\"HAW\">&#257;&hellip;paha", "<span lang=\"HAW\">&#257; &hellip; paha")
+                .Replace("<p><span>nvt.</span> Food or food plant", "<p><span>1.</span> <span>nvt.</span> Food or food plant")
                 // Typos with _
                 .Replace("<span>Na_na_", "<span>N&#257;n&#257;.")
                 .Replace(">Palaki &#699;an_ai</span>", ">Palaki &#699;&#257;nai</span>")
