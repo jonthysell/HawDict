cea851c58063d5572067ab6f7949cfb744c2e772
Jon Thysell <thysell@gmail.com>
2026-09-24 17:04:39 -0700

    Automatically set dictionary date to date of last commit

--- src/HawDict/Input/InputDictBase.cs
+++ src/HawDict/Input/InputDictBase.cs
@@ -147,7 +147,7 @@ namespace HawDict
             metadata.ArticleKeyLangCode = TranslationType == TranslationType.EngToHaw ? "ENG" : "HAW";
             metadata.ArticleValueLangCode = TranslationType == TranslationType.EngToHaw ? "HAW" : "ENG";
             metadata.FileVersion = AppInfo.Version;
-            metadata.CreationDateTime = new DateTime(2025, 7, 23); // TODO update before release
+            metadata.CreationDateTime = DateTime.TryParse(ThisAssembly.Git.CommitDate, out var commitDate) ? commitDate : DateTime.Now;
 
             return metadata;
         }
